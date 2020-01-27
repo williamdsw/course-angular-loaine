4b1831f628bb9fb474495b6732f8e00351ffb6a0
William Santos <williamdsw@outlook.com>
2020-01-27 17:45:08 -0300

    003 - Modulos (ngModule)

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -5,18 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuSegundoComponent } from './meu-segundo/meu-segundo.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
+  // Lista todos components, diretivas e pipes
   declarations: [
     AppComponent,
     MeuPrimeiroComponent,
     MeuSegundoComponent
   ],
+  // Lista todos modulos
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
+  // Lista todos servicos disponiveis para todos componentes
   providers: [],
+
+  // Qual componente que deve ser o container da aplicacao (primeiro)
   bootstrap: [AppComponent]
 })
 export class AppModule { }
