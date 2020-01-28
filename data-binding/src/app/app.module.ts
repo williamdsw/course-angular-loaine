a5a8aefb6b9dd8aa467f05ada91302ee00c4c7df
William Santos <williamdsw@outlook.com>
2020-01-28 15:59:37 -0300

    008 - Event binding

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { ClassStyleBindingComponent } from './class-style-binding/class-style-binding.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent,
-    ClassStyleBindingComponent
+    ClassStyleBindingComponent,
+    EventBindingComponent
   ],
   imports: [
     BrowserModule
---
+++ data-binding/src/app/event-binding/event-binding.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-binding',
+  templateUrl: './event-binding.component.html',
+  styleUrls: ['./event-binding.component.css']
+})
+export class EventBindingComponent {
+
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
+
+  constructor() { }
+
+  botaoClicado() {
+    alert ('Botão clicado');
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    const input = (event.target as HTMLInputElement);
+    this.valorAtual = input.value;
+  }
+
+  salvarValor(valor) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+}
