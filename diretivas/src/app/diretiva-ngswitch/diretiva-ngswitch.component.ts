83f1c71a80d3e6c4c1c3da8d9c27ab1de19c9f27
William Santos <williamdsw@outlook.com>
2020-01-29 16:00:40 -0300

    021 - Diretivas: ngSwitch, ngSwitchCase e ngSwitchDefault

---
+++ diretivas/src/app/diretiva-ngswitch/diretiva-ngswitch.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngswitch',
+  templateUrl: './diretiva-ngswitch.component.html',
+  styleUrls: ['./diretiva-ngswitch.component.scss']
+})
+export class DiretivaNgswitchComponent implements OnInit {
+
+  aba: string;
+
+  constructor() {
+    this.aba = 'home';
+  }
+
+  ngOnInit() {
+  }
+
+}
