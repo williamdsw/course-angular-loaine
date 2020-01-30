a86d366e6e05b2036e1fb3c60790aaa2e3bb7e15
William Santos <williamdsw@outlook.com>
2020-01-30 16:47:34 -0300

    028 - Criando uma diretiva de atributo

---
+++ diretivas/src/app/shared/fundo-amarelo.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: 'p[appFundoAmarelo]'
+})
+export class FundoAmareloDirective {
+
+  // ElementRef = referencia ao elemento que usa essa diretiva
+  // Renderer = Modifica propriedades do elemento, mas de forma mais segura que ElementRef
+  constructor(private elementRef: ElementRef,
+              private renderer: Renderer2) {
+    // console.log (this.elementRef);
+    // this.elementRef.nativeElement.style.backgroundColor = 'yellow';
+    this.renderer.setStyle (this.elementRef.nativeElement, 'background-color', 'yellow');
+   }
+
+}
