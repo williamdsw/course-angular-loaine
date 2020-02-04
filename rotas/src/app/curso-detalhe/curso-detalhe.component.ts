0df07265162c5259aa8013ec36e6a20008105f14
William Santos <williamdsw@outlook.com>
2020-02-04 14:55:07 -0300

    049 - Rotas: Escutando mudancas nos parametros de roteamento

--- rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,21 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
   templateUrl: './curso-detalhe.component.html',
   styleUrls: ['./curso-detalhe.component.css']
 })
-export class CursoDetalheComponent implements OnInit {
+export class CursoDetalheComponent implements OnInit, OnDestroy {
+
+  // FIELDS
 
   id: string;
+  inscricao: Subscription;
 
-  constructor(private route: ActivatedRoute) {
-    console.log (this.route);
-    this.id = this.route.snapshot.params['id'];
-   }
+  // CONSTRUCTOR
+
+  constructor(private route: ActivatedRoute) {}
+
+  // LIFE CYCLE HOOKS
 
   ngOnInit() {
+    this.inscricao = this.route.params.subscribe (
+      (params: any) => {
+        this.id = params['id'];
+      }, error => {
+        console.log (error);
+      }
+    );
+  }
+
+  // Desinscreve da inscricao criada
+  ngOnDestroy() {
+    this.inscricao.unsubscribe ();
   }
 
 }
