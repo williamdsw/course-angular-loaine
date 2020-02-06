653620211129a068fb6d1caed0a8b4c015cae461
William Santos <williamdsw@outlook.com>
2020-02-06 16:27:26 -0300

    068 - Forms (template driven) - Inicializando valores com ngModel

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  usuario: any = {
+    nome: 'Frank Zappa',
+    email: 'zappa@email.com'
+  };
+
   constructor() { }
 
   ngOnInit() {
@@ -14,6 +19,7 @@ export class TemplateFormComponent implements OnInit {
 
   onSubmit(form) {
     console.log (form);
+    console.log (this.usuario);
   }
 
 }
