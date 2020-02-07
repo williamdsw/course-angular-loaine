ff828864469e7347216f692d3b8d8158f45d8bbb
William Santos <williamdsw@outlook.com>
2020-02-07 17:17:44 -0300

    077 - Forms (template driven) - Pesquisando endereco automaticamente com CEP

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,25 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-template-form',
   templateUrl: './template-form.component.html',
   styleUrls: ['./template-form.component.css']
 })
-export class TemplateFormComponent implements OnInit {
+export class TemplateFormComponent implements OnInit, OnDestroy {
+
+  // FIELDS
 
   usuario: any = {
     nome: null,
     email: null
   };
 
-  constructor() { }
+  private inscricao: Subscription;
+
+  // CONSTRUCTOR
+
+  constructor(private httpClient: HttpClient) { }
+
+  // LIFE CYCLE HOOKS
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.inscricao.unsubscribe ();
+  }
+
+  // HELPER FUNCTIONS
+
   onSubmit(form) {
-    console.log (form);
-    console.log (this.usuario);
+    console.log(form);
+    console.log(this.usuario);
+  }
+
+  consultaCEP(cep: string) {
+    cep = cep.replace(/\D/g, '');
+
+    if (cep !== '') {
+      const VALIDA_CEP = /^[0-9]{8}$/;
+      if (VALIDA_CEP.test(cep)) {
+        this.inscricao = this.httpClient.get(`//viacep.com.br/ws/${cep}/json`).subscribe(
+          response => {
+            console.log(response);
+          },
+          error => {
+            console.log(error);
+          });
+      }
+    }
   }
 
 }
--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { AngularMaterialModule } from './../angular-material/angular-material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { TemplateFormComponent } from './template-form.component';
 import { FormDebugComponent } from '../form-debug/form-debug.component';
@@ -10,12 +11,13 @@ import { FormDebugComponent } from '../form-debug/form-debug.component';
 @NgModule({
   declarations: [
     TemplateFormComponent,
-    FormDebugComponent
+    FormDebugComponent,
   ],
   imports: [
     CommonModule,
     FormsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    HttpClientModule
   ]
 })
 export class TemplateFormModule { }
