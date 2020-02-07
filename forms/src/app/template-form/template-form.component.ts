dd08b5e415158d7c6a1b912f956cc73ac9ce82a8
William Santos <williamdsw@outlook.com>
2020-02-07 18:16:11 -0300

    078 - Forms (template driven) - Populando campos com setValue e patchValue (CEP)

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -38,21 +38,64 @@ export class TemplateFormComponent implements OnInit, OnDestroy {
     console.log(this.usuario);
   }
 
-  consultaCEP(cep: string) {
+  consultaCEP(cep: string, form) {
     cep = cep.replace(/\D/g, '');
 
     if (cep !== '') {
       const VALIDA_CEP = /^[0-9]{8}$/;
       if (VALIDA_CEP.test(cep)) {
-        this.inscricao = this.httpClient.get(`//viacep.com.br/ws/${cep}/json`).subscribe(
-          response => {
-            console.log(response);
-          },
-          error => {
-            console.log(error);
-          });
+
+        this.resetaDadosFormulario (form);
+
+        this.inscricao = this.httpClient.get(`//viacep.com.br/ws/${cep}/json`).subscribe (
+          response => { this.populaDadosForm (response, form); },
+          error => { console.log(error); });
       }
     }
+  }
+
+  populaDadosForm(dados, formulario) {
+
+    if (!('erro' in dados)) {
+      /*
+      formulario.setValue ({
+        nome: formulario.value.nome,
+        email: formulario.value.email,
+        endereco: {
+          rua: dados.logradouro,
+          cep: dados.cep,
+          numero: formulario.value.endereco.numero,
+          complemento: dados.complemento,
+          bairro: dados.bairro,
+          cidade: dados.localidade,
+        }
+      });
+      */
+
+      formulario.form.patchValue({
+        endereco: {
+          rua: dados.logradouro,
+          cep: dados.cep,
+          complemento: dados.complemento,
+          bairro: dados.bairro,
+          cidade: dados.localidade,
+        }
+      });
+    } else {
+      this.resetaDadosFormulario (formulario);
+      alert ('CEP não encontrado!');
+    }
+  }
+
+  resetaDadosFormulario(formulario) {
+    formulario.form.patchValue ({
+      endereco: {
+        rua: null,
+        complemento: null,
+        bairro: null,
+        cidade: null
+      }
+    });
   }
 
 }
