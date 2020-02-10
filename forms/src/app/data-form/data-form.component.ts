617e3063973da764ace6031e8b5c8460b58609aa
William Santos <williamdsw@outlook.com>
2020-02-10 14:21:03 -0300

    088 - Formularios reativos: Form groups (agrupando dados)

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -34,13 +34,16 @@ export class DataFormComponent implements OnInit, OnDestroy {
     this.formulario = this.formBuilder.group({
       nome: [null, [ Validators.required, Validators.minLength (3), Validators.maxLength (20) ]],
       email: [null, [ Validators.required, Validators.email ]],
-      cep: [null, [ Validators.required ]],
-      numero: [null, [ Validators.required ]],
-      complemento: [null],
-      rua: [null, [ Validators.required ]],
-      bairro: [null, [ Validators.required ]],
-      cidade: [null, [ Validators.required ]],
-      estado: [null, [ Validators.required ]],
+
+      endereco: this.formBuilder.group ({
+        cep: [null, [ Validators.required ]],
+        numero: [null, [ Validators.required ]],
+        complemento: [null],
+        rua: [null, [ Validators.required ]],
+        bairro: [null, [ Validators.required ]],
+        cidade: [null, [ Validators.required ]],
+        estado: [null, [ Validators.required ]],
+      })
     });
 
   }
