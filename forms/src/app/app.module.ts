610613dee85c2b04abd3271fa88ed3608e30b1bb
William Santos <williamdsw@outlook.com>
2020-02-10 11:59:50 -0300

    080 - Formularios reativos: Configuracao (Modulo e Componente)

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,25 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { DataFormModule } from './data-form/data-form.module';
 import { TemplateFormModule } from './template-form/template-form.module';
 
 import { AppComponent } from './app.component';
-import { DataFormComponent } from './data-form/data-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataFormComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     AngularMaterialModule,
-    TemplateFormModule
+    TemplateFormModule,
+    DataFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
