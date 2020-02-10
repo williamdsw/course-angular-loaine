5db1631fc9793a827c7a80f9432eb11ea1f03538
William Santos <williamdsw@outlook.com>
2020-02-10 13:43:42 -0300

    085 - Formularios reativos: Aplicando validacao nos campos

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs';
 
@@ -31,16 +31,16 @@ export class DataFormComponent implements OnInit, OnDestroy {
 
     this.inscricao = new Subscription();
 
-    // 1a forma
-    /*this.formulario = new FormGroup ({
-      nome: new FormControl (null),
-      email: new FormControl (null)
-    });*/
-
-    // 2a forma
     this.formulario = this.formBuilder.group({
-      nome: [null],
-      email: [null],
+      nome: [null, [
+        Validators.required,
+        Validators.minLength (3),
+        Validators.maxLength (20),
+      ]],
+      email: [null, [
+        Validators.required,
+        Validators.email
+      ]],
     });
 
   }
