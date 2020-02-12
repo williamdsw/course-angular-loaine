9d5a490d0f5ef5653967279b3bbfeb41060b59cd
williamdsw <williamdsw@outlook.com>
2020-02-12 17:36:08 -0300

    Atualizado angular/material

--- forms/src/app/angular-material/angular-material.module.ts
+++ forms/src/app/angular-material/angular-material.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
-import {
-  MatCardModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatSelectModule,
-  MatCheckboxModule,
-  MatRadioModule,
-  MatAutocompleteModule,
-  MatDatepickerModule,
-  MatMenuModule,
-  MatSidenavModule,
-  MatToolbarModule,
-  MatButtonModule,
-  MatTabsModule
-} from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const MATERIAL_COMPONENTS = [
   MatCardModule,
