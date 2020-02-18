06252ca46c5360f7755dfafbe6504f0bdf78123c
williamdsw <williamdsw@outlook.com>
2020-02-18 14:47:57 -0300

    119 - Http: Criando formulario para criar e editar cursos

--- requests-http/src/app/cursos/cursos-routing.module.ts
+++ requests-http/src/app/cursos/cursos-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
+import { CursosFormComponent } from './cursos-form/cursos-form.component';
 
 const ROUTES: Routes = [
-  { path: '', component: CursosListaComponent }
+  { path: '', component: CursosListaComponent },
+  { path: 'novo', component: CursosFormComponent },
+  { path: 'editar/:id', component: CursosFormComponent }
 ];
 
 @NgModule({
