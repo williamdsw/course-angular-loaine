f9bf10a1722a54b62d4e8c626166732eaed5060b
William Santos <williamdsw@outlook.com>
2020-11-07 19:19:50 -0300

    Updating CDK and Material of Rotas to latest version

--- rotas/src/app/material/material.module.ts
+++ rotas/src/app/material/material.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
-import {
-  MatToolbarModule,
-  MatSidenavModule,
-  MatListModule,
-  MatButtonModule,
-  MatIconModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatCardModule,
-  MatGridListModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const MATERIAL_COMPONENTS = [
   MatToolbarModule,
