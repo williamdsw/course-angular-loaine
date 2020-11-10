81c2de03fbae3a54d15d06e4ba72b0f6dc1113c3
William Santos <williamdsw@outlook.com>
2020-11-10 20:25:10 -0300

    Code refactoring for App Component

--- requests-http/src/app/app-routing.module.ts
+++ requests-http/src/app/app-routing.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const ROUTES: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'reactive-search' },
-  { 
-    path: 'cursos', 
-    loadChildren: () => import ('./cursos/cursos.module').then (mod => mod.CursosModule) },
-  { 
+  {
+    path: '', pathMatch: 'full', redirectTo: 'reactive-search'
+  },
+  {
+    path: 'cursos',
+    loadChildren: () => import('./cursos/cursos.module').then(mod => mod.CursosModule)
+  },
+  {
     path: 'rxjs-poc',
     loadChildren: () => import ('./unsubscribe-rxjs/unsubscribe-rxjs.module').then (mod => mod.UnsubscribeRxjsModule)
   },
-  { 
+  {
     path: 'upload-file',
     loadChildren: () => import ('./upload-file/upload-file.module').then (mod => mod.UploadFileModule)
   },
-  { 
+  {
     path: 'reactive-search',
     loadChildren: () => import ('./reactive-search/reactive-search.module').then (mod => mod.ReactiveSearchModule)
   }
--- requests-http/src/app/app.module.ts
+++ requests-http/src/app/app.module.ts
@@ -6,9 +6,9 @@ import { ModalModule } from 'ngx-bootstrap/modal';
 import { AppRoutingModule } from './app-routing.module';
 import { UnsubscribeRxjsModule } from './unsubscribe-rxjs/unsubscribe-rxjs.module';
 
-import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
